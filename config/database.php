2335cddfcfed4fa622d1369d8db8e9bfd53d61b5
DYLLAN BROOKLYN SALAS CRUZ <155581701+DilanSalas@users.noreply.github.com>
2025-05-30 08:16:58 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,23 +1,33 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "apimetodos";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+    private $conn;
 
     public function getConnection() {
         $this->conn = null;
+
+        $url = "mysql://dilansalas200@gmail.com:qx7u83jvoxcfkanb@metodos-metodosbd-ljnklx:3306/apimetodos";
+
+        $url = rtrim($url, '}');
+
+        $parts = parse_url($url);
+
+        $host = $parts['host'];
+        $port = $parts['port'];
+        $user = $parts['user'];
+        $pass = $parts['pass'];
+        $dbname = ltrim($parts['path'], '/');
+
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password
+                "mysql:host=$host;port=$port;dbname=$dbname",
+                $user,
+                $pass
             );
             $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
+        } catch (PDOException $exception) {
             echo "Error de conexión: " . $exception->getMessage();
         }
+
         return $this->conn;
     }
 }
