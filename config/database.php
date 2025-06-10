f60d98bcaf43eeecd09b5af97e4bccc318cae70a
CristoferRojasCode <cristoferrojasw@gmail.com>
2025-06-10 11:24:09 -0600

    cambio en database.php
    cambio en database.php

--- config/database.php
+++ config/database.php
@@ -1,28 +1,24 @@
 <?php
 class Database {
-    private $conn;
+    private $host = "145.223.74.28";
+    private $port = "3311";
+    private $db_name = "quejas_taxi";
+    private $username = "root";
+    private $password = "123";
+    public $conn;
 
     public function getConnection() {
         $this->conn = null;
-
-        // Evitar el uso de parse_url con @ en el usuario
-        $host = "metodos-metodosbd-ljnklx";
-        $port = 3306;
-        $user = "dilansalas200@gmail.com";
-        $pass = "qx7u83jvoxcfkanb";
-        $dbname = "apimetodos";
-
         try {
             $this->conn = new PDO(
-                "mysql:host=$host;port=$port;dbname=$dbname",
-                $user,
-                $pass
+                "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
             );
             $this->conn->exec("set names utf8");
-        } catch (PDOException $exception) {
+        } catch(PDOException $exception) {
             echo "Error de conexión: " . $exception->getMessage();
         }
-
         return $this->conn;
     }
 }
