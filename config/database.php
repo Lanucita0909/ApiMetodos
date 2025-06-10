55b6e232206646bf847f348d0652eac5e78f5bed
Dilan <dilansalas200@gmail.com>
2025-06-10 15:01:06 -0600

    Revert "cambio en database.php"
    This reverts commit f60d98bcaf43eeecd09b5af97e4bccc318cae70a.

--- config/database.php
+++ config/database.php
@@ -1,24 +1,28 @@
 <?php
 class Database {
-    private $host = "145.223.74.28";
-    private $port = "3311";
-    private $db_name = "quejas_taxi";
-    private $username = "root";
-    private $password = "123";
-    public $conn;
+    private $conn;
 
     public function getConnection() {
         $this->conn = null;
+
+        // Evitar el uso de parse_url con @ en el usuario
+        $host = "metodos-metodosbd-ljnklx";
+        $port = 3306;
+        $user = "dilansalas200@gmail.com";
+        $pass = "qx7u83jvoxcfkanb";
+        $dbname = "apimetodos";
+
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password
+                "mysql:host=$host;port=$port;dbname=$dbname",
+                $user,
+                $pass
             );
             $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
+        } catch (PDOException $exception) {
             echo "Error de conexión: " . $exception->getMessage();
         }
+
         return $this->conn;
     }
 }
