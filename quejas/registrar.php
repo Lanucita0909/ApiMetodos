e7e5272af131f7e42e50afc83931c6af3db41b73
Brandon Zarate Mairena <108448549+ZarateBM@users.noreply.github.com>
2025-06-13 10:23:33 -0600

    Update registrar.php

--- quejas/registrar.php
+++ quejas/registrar.php
@@ -1,6 +1,6 @@
 <?php
 // Configuración CORS completa - añade estos encabezados
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://powderblue-termite-675995.hostingersite.com");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
